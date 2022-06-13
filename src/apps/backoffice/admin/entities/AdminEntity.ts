6bb169b05195f8d432e4e6b7e9918d07a4e1261d
Fernando Fernandez <frfernandezdev@gmail.com>
2022-06-13 19:42:41 -0400

    feat(apps/backoffice/admin): added AdminEntity

---
+++ src/apps/backoffice/admin/entities/AdminEntity.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { BackofficeAdminRoles } from 'src/contexts/backoffice/admin/domain/BackofficeAdminRole';
+
+export class Admin {
+  @ApiProperty()
+  id: string;
+  @ApiProperty()
+  displayName: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  phoneNumber: string;
+  @ApiProperty()
+  photoURL: string;
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  lastname: string;
+  @ApiProperty()
+  disabled: boolean;
+  @ApiProperty({
+    enum: BackofficeAdminRoles,
+  })
+  role: number;
+}
