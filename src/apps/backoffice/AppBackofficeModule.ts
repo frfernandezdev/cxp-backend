3edaab3fb359f90e1597df998897652e5d72dcf6
Fernando Fernandez <frfernandezdev@gmail.com>
2022-06-12 20:20:14 -0400

    feat(apps/backoffice): add providers to module

--- src/apps/backoffice/AppBackofficeModule.ts
+++ src/apps/backoffice/AppBackofficeModule.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BackofficeAdminModule } from 'src/contexts/backoffice/admin/BackofficeAdminModule';
 import { BackofficeModule } from 'src/contexts/backoffice/BackofficeModule';
+import { BackofficeMethodModule } from 'src/contexts/backoffice/method/BackofficeMethodModule';
+import { BackofficePlanModule } from 'src/contexts/backoffice/plan/BackofficePlanModule';
+import { BackofficeSpecialityModule } from 'src/contexts/backoffice/speciality/BackofficeSpecialityModule';
+import { BackofficeUserModule } from 'src/contexts/backoffice/user/BackofficeUserModule';
 import { AppBackofficeAdminModule } from './admin/AppBackofficeAdminModule';
 import { AppBackofficeMethodModule } from './method/AppBackofficeMethodModule';
 import { AppBackofficePlanModule } from './plan/AppBackofficePlanModule';
@@ -14,6 +19,13 @@ import { AppBackofficeUserModule } from './user/AppBackofficeUserModule';
     AppBackofficePlanModule,
     AppBackofficeSpecialityModule,
     AppBackofficeUserModule,
+  ],
+  providers: [
+    BackofficeAdminModule,
+    BackofficeMethodModule,
+    BackofficePlanModule,
+    BackofficeSpecialityModule,
+    BackofficeUserModule,
   ],
   exports: [
     AppBackofficeAdminModule,
