3c046998da9f2207fad516ca6e4d132c8b586ea2
Fernando Fernandez <frfernandezdev@gmail.com>
2022-06-12 20:27:01 -0400

    feat(npm): added compression and axios

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import helmet from 'helmet';
 import { VersioningType } from '@nestjs/common';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.use(compression());
+
   app.enableCors();
   app.enableVersioning({
     type: VersioningType.URI,
