71a3544894b1acdcea3a55b9b8a1b29382dc26e0
Fernando Fernandez <frfernandezdev@gmail.com>
2022-06-13 19:34:31 -0400

    feat(npm): added swagger and config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './AppModule';
 import helmet from 'helmet';
 import { VersioningType } from '@nestjs/common';
 import * as compression from 'compression';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
   app.setGlobalPrefix('v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('ConeXperto')
+    .setDescription('ConeXperto - API Restful Docs')
+    .setVersion('1.0.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/v1/docs', app, document);
 
   await app.listen(3000);
 }
