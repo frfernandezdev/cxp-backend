689106bb0996af0478c716712077875f5d42092b
Fernando Fernandez <frfernandezdev@gmail.com>
2022-06-12 20:18:37 -0400

    feat(npm): added helmet and versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
+import helmet from 'helmet';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.setGlobalPrefix('v1');
+
   await app.listen(3000);
 }
 bootstrap();
